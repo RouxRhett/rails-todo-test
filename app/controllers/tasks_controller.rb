b63bca70be30e31f1d49f4c1e324e200c84f4b79
RouxRhett <nsch.4871@gmail.com>
2021-09-30 23:17:32 +0900

    [add]Create a page for layout testing. Pages added at this time will be deleted later.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,8 @@ class TasksController < ApplicationController
     @msg = '現在登録されているタスク一覧です。'
     @data = Task.all
   end
+
+  def test
+    @msg = 'ここは削除されます。'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'tasks/index'
   get '/', to: 'tasks#index'
+  get '/2', to: 'tasks#test'#削除予定
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
