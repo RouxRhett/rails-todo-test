04e701702c6a4a52625663b0e925a353a2271ddf
RouxRhett <nsch.4871@gmail.com>
2021-10-01 22:49:42 +0900

    [add]Created an edit page.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @msg = 'List'
+    @msg = ''
     @data = Task.all
   end
 
@@ -10,5 +10,32 @@ class TasksController < ApplicationController
   end
 
   def add
+    @msg ='Added a task.'
+  end
+
+  def create
+    if request.post? then
+      obj = Task.create(
+        title:params['title'],
+        content:params['content']
+      )
+    end
+    redirect_to '/'
+  end
+
+  def edit
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    obj = Task.find(params[:id])
+    obj.update(task_params)
+    redirect_to '/'
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
