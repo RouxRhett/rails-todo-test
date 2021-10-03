90f3c379d4349f9bd7413d44284ed9303bbd18ff
RouxRhett <nsch.4871@gmail.com>
2021-10-03 19:32:49 +0900

    [change]Change path specification.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+
   get 'tasks/index'
   get '/', to: 'tasks#index'
 
-  get '/add', to: 'tasks#add'
-  post '/add', to: 'tasks#create' 
+  get '/add', to: 'tasks#add', as: 'add'
+  post '/add', to: 'tasks#create'
 
-  get '/:id', to: 'tasks#show'
-  delete '/:id', to: 'tasks#delete'
+  get '/:id', to: 'tasks#show', as: 'show'
+  delete '/:id', to: 'tasks#delete', as: 'delete'
 
-  get '/edit/:id', to: 'tasks#edit'
+  get '/edit/:id', to: 'tasks#edit', as:'edit'
   post '/edit/:id', to: 'tasks#update'
   patch '/edit/:id', to: 'tasks#update'
 
